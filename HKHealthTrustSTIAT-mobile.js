effffd9ef375606f6d6f5ae863862a75db009de7
Jacky-Liangjq <jacky.leong.ljq@gmail.com>
2026-05-08 13:15:00 +0800

    Use relative task dependency paths

--- HKHealthTrustSTIAT-mobile.js
+++ HKHealthTrustSTIAT-mobile.js
@@ -1,7 +1,7 @@
 define(
   [
     'pipAPI',
-    'https://cdn.jsdelivr.net/gh/Jacky-Liangjq/zh-TW-ST-IAT-HK-Health-Institution-Trust-Formal@v1.0.5/qstiat-formal.js'
+    './qstiat-formal.js'
   ],
   function (APIConstructor, stiatExtension) {
     return stiatExtension({
